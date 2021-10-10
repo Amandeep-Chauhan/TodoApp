113db942df392cbb3dbe91285e3a568af02206c8
Amandeep-Chauhan <amandeepchauhan258@gmail.com>
2021-10-10 23:33:39 +0530

    added delete and edit features

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import "./app.styles.scss";
+import { useState } from "react";
 import TodoList from "./components/todoList";
+
+const getLocalItems = () => {
+  const lists = localStorage.getItem("lists");
+  const retrievedLists = lists ? JSON.parse(localStorage.getItem("lists")) : [];
+  return retrievedLists;
+};
 
 export default function App() {
   const [input, setInput] = useState();
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalItems());
+  const [isEditOn, setIsEditOn] = useState(false);
+  const [toEdit, setToEdit] = useState(null);
 
   const handleChange = (e) => {
-    setInput(e.target.value);
+    const value = e.target.value;
+    setInput(value);
   };
 
+  useEffect(() => {
+    localStorage.setItem("lists", JSON.stringify(items));
+  }, [items]);
+  useEffect(() => {}, [isEditOn]);
   const handlesubmit = (e) => {
     e.preventDefault();
-    setItems((prevItems) => {
-      return [...prevItems, input];
-    });
+    if (isEditOn) {
+      if (!input) {
+        alert("Enter a todo before Saving");
+      } else {
+        const newInput = items.map((item) => {
+          if (toEdit !== null && toEdit === item.id) {
+            return { ...item, name: input };
+          }
+          return item;
+        });
+        setToEdit(null);
+        setIsEditOn(false);
+        setItems(newInput);
+      }
+    } else {
+      if (!input) {
+        alert("Enter a todo before adding");
+      } else {
+        const allInput = { id: Date.now(), name: input };
+        setItems([...items, allInput]);
+      }
+    }
     setInput("");
   };
 
-  const deleteItem = (id) => {
-    console.log("deleted");
+  const handleEdit = (id, value) => {
+    setIsEditOn(true);
+    setToEdit(id);
+    setInput(value);
+  };
 
-    setItems((prevItems) => {
-      return prevItems.filter((arrItems, idx) => {
-        return idx !== id;
+  // delete items in todo list
+  const deleteItem = (id) => {
+    setItems((items) => {
+      return items.filter((arrItems) => {
+        return arrItems.id !== id;
       });
     });
   };
 
-  // const randomId = { id: new Date().getTime().toString() };
+  // clearall
+  const handleClearAll = () => {
+    setItems([]);
+  };
 
   return (
     <div className="todo-app">
-      <h1>todo list </h1>
+      <h2>What's the Plan for Today ?</h2>
       <form className="todo-form" onSubmit={handlesubmit} autoComplete="off">
         <input
           type="text"
@@ -43,19 +84,35 @@ export default function App() {
           className="todo-input"
         />
         <button type="submit" className="todo-button">
-          Add todo
+          {isEditOn ? "Save Todo" : "Add Todo"}
         </button>
       </form>
-      <ol>
-        {items.map((item, idx) => (
-          <TodoList
-            key={idx}
-            id={idx}
-            itemText={item}
-            handleDelete={deleteItem}
-          />
-        ))}
-      </ol>
+      {items.length > 0 ? (
+        <ul className="item-list-wrapper">
+          <div className="scroll-wrapper">
+            {items.map((elem) => (
+              <TodoList
+                key={elem.id}
+                id={elem.id}
+                itemText={elem.name}
+                handleEdit={handleEdit}
+                handleDelete={deleteItem}
+              />
+            ))}
+          </div>
+          {items.length > 1 ? (
+            <div className="clearall">
+              <button className="clearall-button" onClick={handleClearAll}>
+                Clear All
+              </button>
+            </div>
+          ) : null}
+        </ul>
+      ) : (
+        <div className="intro">
+          <p className="intro-text">welcome to my to-do app</p>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,17 +1,23 @@
 import React from "react";
+import "./todoList.styles.scss";
+import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 
-function TodoList({ itemText, handleDelete, id }) {
+function TodoList({ itemText, handleDelete, id, handleEdit }) {
   return (
-    <>
-      <li className="todo-list">
-        {itemText}
-        <RiDeleteBin5Fill
-          title="Delete item"
-          onClick={() => handleDelete(id)}
-        />
-      </li>
-    </>
+    <span className="todo-list">
+      <li className="todo-list_text">{itemText}</li>
+      <FaRegEdit
+        onClick={() => handleEdit(id, itemText)}
+        className="todo-List_edit"
+        title="Edit item"
+      />
+      <RiDeleteBin5Fill
+        className="todo-List_delete"
+        title="Delete item"
+        onClick={() => handleDelete(id)}
+      />
+    </span>
   );
 }
 
